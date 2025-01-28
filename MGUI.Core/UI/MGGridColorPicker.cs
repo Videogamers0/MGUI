f164d00467563991a0b1e7f4b3758d9d178130d3
Kevin Augustino <kevinaugustino@hotmail.com>
2025-01-27 22:14:45 -0500

    Add more ColorPalettes to MGGridColorPicker.

--- MGUI.Core/UI/MGGridColorPicker.cs
+++ MGUI.Core/UI/MGGridColorPicker.cs
@@ -54,9 +54,25 @@ namespace MGUI.Core.UI
         /// <summary>64 colors:<br/><see href="https://en.wikipedia.org/wiki/Enhanced_Graphics_Adapter#Color_palette"/><para/>
         /// Colors are ordered according to: <see href="https://upload.wikimedia.org/wikipedia/commons/0/02/EGA_Table.svg"/> and <see href="https://en.wikipedia.org/wiki/File:EGA64_Full_Palette.png"/>.<para/>
         /// This palette is often displayed with 8 columns, 8 rows.</summary>
-        EGA_64
-
-        //Maybe also: Atari2600_NTSC, NES, SegaMasterSystem, Intellivision, SuperCassetteVision, Odyssey2, https://en.wikipedia.org/wiki/List_of_video_game_console_palettes
+        EGA_64,
+        /// <summary>128 colors used by the Atari 2600 NTSC format:<br/>
+        /// <see href="https://en.wikipedia.org/wiki/List_of_video_game_console_palettes#Atari_2600"/><para/>
+        /// This palette is often displayed with 16 columns, 8 rows.</summary>
+        Atari2600_NTSC,
+        /// <summary>52 colors used by the Nintendo Entertainment System:<br/>
+        /// <see href="https://en.wikipedia.org/wiki/List_of_video_game_console_palettes#Nintendo_Entertainment_System"/><para/>
+        /// This palette is often displayed with 13 columns, 4 rows<para/>
+        /// Note: The duplicate blacks and whites have been omitted, and only 1 dark gray and 1 light gray are included in this palette, so it's not exactly the same as the wikipedia page shows.</summary>
+        NES,
+        /// <summary>16 colors:<br/>
+        /// <see href="https://en.wikipedia.org/wiki/List_of_video_game_console_palettes#Intellivision"/><para/>
+        /// Colors are ordered according to: <see href="https://en.wikipedia.org/wiki/File:Intellivision_Palette.png"/><para/>
+        /// This palette is often displayed with 8 columns, 2 rows.</summary>
+        Intellivision,
+        /// <summary>16 colors:<br/>
+        /// <see href="https://en.wikipedia.org/wiki/List_of_video_game_console_palettes#Super_Cassette_Vision"/><para/>
+        /// This palette is often displayed with 4 columns, 4 rows.</summary>
+        SuperCassetteVision
     }
 
     public static class ColorPalettes
@@ -73,7 +89,11 @@ namespace MGUI.Core.UI
             { ColorPalette.Windows_16, 8 },
             { ColorPalette.Windows_20, 10 },
             { ColorPalette.Mac_16, 8 },
-            { ColorPalette.EGA_64, 8 }
+            { ColorPalette.EGA_64, 8 },
+            { ColorPalette.Atari2600_NTSC, 16 },
+            { ColorPalette.NES, 13 },
+            { ColorPalette.Intellivision, 8 },
+            { ColorPalette.SuperCassetteVision, 4 }
         };
 
         private static readonly IReadOnlyDictionary<ColorPalette, IReadOnlyList<string>> Colors = new Dictionary<ColorPalette, IReadOnlyList<string>>()
@@ -161,6 +181,54 @@ namespace MGUI.Core.UI
                     "#550055", "#5500FF", "#55AA55", "#55AAFF", "#FF0055", "#FF00FF", "#FFAA55", "#FFAAFF",
                     "#555500", "#5555AA", "#55FF00", "#55FFAA", "#FF5500", "#FF55AA", "#FFFF00", "#FFFFAA",
                     "#555555", "#5555FF", "#55FF55", "#55FFFF", "#FF5555", "#FF55FF", "#FFFF55", "#FFFFFF"
+                }
+            },
+            {
+                ColorPalette.Atari2600_NTSC,
+                new List<string>()
+                {
+                    "#000000", "#444400", "#702800", "#841800", "#880000", "#78005c", "#480078", "#140084",
+                    "#000088", "#00187c", "#002c5c", "#00402c", "#003c00", "#143800", "#2c3000", "#442800",
+                    "#404040", "#646410", "#844414", "#983418", "#9c2020", "#8c2074", "#602090", "#302098",
+                    "#1c209c", "#1c3890", "#1c4c78", "#1c5c48", "#205c20", "#345c1c", "#4c501c", "#644818",
+                    "#6c6c6c", "#848424", "#985c28", "#ac5030", "#b03c3c", "#a03c88", "#783ca4", "#4c3cac",
+                    "#3840b0", "#3854a8", "#386890", "#387c64", "#407c40", "#507c38", "#687034", "#846830",
+                    "#909090", "#a0a034", "#ac783c", "#c06848", "#c05858", "#b0589c", "#8c58b8", "#6858c0",
+                    "#505cc0", "#5070bc", "#5084ac", "#509c80", "#5c9c5c", "#6c9850", "#848c4c", "#a08444",
+                    "#b0b0b0", "#b8b840", "#bc8c4c", "#d0805c", "#d07070", "#c070b0", "#a070cc", "#7c70d0",
+                    "#6874d0", "#6888cc", "#689cc0", "#68b494", "#74b474", "#84b468", "#9ca864", "#b89c58",
+                    "#c8c8c8", "#d0d050", "#cca05c", "#e09470", "#e08888", "#d084c0", "#b484dc", "#9488e0",
+                    "#7c8ce0", "#7c9cdc", "#7cb4d4", "#7cd0ac", "#8cd08c", "#9ccc7c", "#b4c078", "#d0b46c",
+                    "#dcdcdc", "#e8e85c", "#dcb468", "#eca880", "#eca0a0", "#dc9cd0", "#c49cec", "#a8a0ec",
+                    "#90a4ec", "#90b4ec", "#90cce8", "#90e4c0", "#a4e4a4", "#b4e490", "#ccd488", "#e8cc7c",
+                    "#ececec", "#fcfc68", "#fcbc94", "#fcb4b4", "#ecb0e0", "#d4b0fc", "#bcb4fc", "#a4b8fc",
+                    "#a4c8fc", "#a4e0fc", "#a4fcd4", "#b8fcb8", "#c8fca4", "#e0ec9c", "#fce08c", "#ffffff"
+                }
+            },
+            {
+                ColorPalette.NES,
+                new List<string>()
+                {
+                    "#000000", "#002a88", "#1412a8", "#3b00a4", "#5c007e", "#6e0040", "#6c0700", "#571d00", "#343500", "#0c4900", "#005200", "rgb(0,68,68)", "rgb(0,68,102)",
+                    "#666666", "#155fda", "#4240fe", "#7627ff", "#a11bcd", "#b81e7c", "#b53220", "#994f00", "#6c6e00", "#388700", "#0d9400", "rgb(0,102,85)", "rgb(0,85,136)",
+                    "#aeaeae", "#64b0fe", "#9390fe", "#c777fe", "#f36afe", "#fe6ecd", "#fe8270", "#eb9f23", "#bdbf00", "#89d900", "#5de530", "#45e182", "#48cedf",
+                    "#ffffff", "#c1e0fe", "#d4d3fe", "#e9c8fe", "#fbc3fe", "#fec5eb", "#fecdc6", "#f7d9a6", "#e5e695", "#d0f097", "#bef5ab", "#b4f3cd", "#b5ecf3"
+                }
+            },
+            {
+                ColorPalette.Intellivision,
+                new List<string>()
+                {
+                    "#0c0005", "#002dff", "#ff3e00", "#c9d464", "#00780f", "#00a720", "#faea27", "#fffcff",
+                    "#a7a8a8", "#5acbff", "#ffa600", "#3c5800", "#ff3276", "#bd95ff", "#6ccd30", "#c81a7d"
+                }
+            },
+            {
+                ColorPalette.SuperCassetteVision,
+                new List<string>()
+                {
+                    "#000000", "#ff0000", "#ffa100", "#ffa09f", "#ffff00", "#a3a000", "#00a100", "#00ff00",
+                    "#a0ff9d", "#00009b", "#0000ff", "#a200ff", "#ff00ff", "#00ffff", "#a2a19f", "#ffffff"
                 }
             }
         };
